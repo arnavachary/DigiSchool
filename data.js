85ed421a50e832a1a97145501a1cc28067ef8181
arnavachary <achary.sureshachary@gamail.com>
2022-01-03 15:58:23 +0530

    Online School

---
+++ data.js
@@ -0,0 +1,9 @@
+var logoUrl =
+  "https://res.cloudinary.com/dxkf8bjwl/image/upload/v1640620932/Digi%20School/Blue_with_Gold_Laurel_Education_Logo__1_-removebg-preview_xmru9i.png";
+
+// Creating Logo
+
+let logoImage = document.getElementById("logo")
+logoImage.src = logoUrl
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("welcome")
+
+
+let body = document.getElementById("body")
+
+
+let navbar = document.getElementById("navbar")
+
+
+
+let about = document.getElementById("about");
+console.log(navbar)
+
+about.append(navbar)
